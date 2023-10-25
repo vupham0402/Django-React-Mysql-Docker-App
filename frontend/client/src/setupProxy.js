d49d8ef3a790c26e881437d1efb1f413c3b25e58
vupham0402 <vupham0402@gmail.com>
2023-10-25 14:42:04 -0400

    fixed proxy

--- frontend/client/src/setupProxy.js
+++ frontend/client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
 	app.use(
 		'/api',
 		createProxyMiddleware({
-			target: 'http://marketexpress',
+			target: 'http://18.206.158.83:5000',
 			changeOrigin: true,
 			ws: true, // Enable WebSocket support
 		})
