9b996e20608cdd84be8bc8244f38769524d012b1
vupham0402 <vupham0402@gmail.com>
2023-10-25 12:25:50 -0400

    edited public ip

--- frontend/client/src/setupProxy.js
+++ frontend/client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
 	app.use(
 		'/api',
 		createProxyMiddleware({
-			target: 'http://3.90.35.167:5000',
+			target: 'http://18.205.190.202:5000',
 			changeOrigin: true,
 		})
 	);
